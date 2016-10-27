7125c3b9fa32edb360b5635697571177a2686a1c
Jordan Schatzman <Jordan Schatzman>
2016-10-26 22:40:57 -0700

    adding check empty function

--- school.py
+++ school.py
@@ -1,8 +1,11 @@
 
+import sys
+import random
+
 schoolname = "Jordan's School"
 
 #Import TSV data as source of all students, GPAs, grades and teachers
-studentdata = [tuple(line.strip().split('\t')) for line in open('studentstsv.txt','r')]
+studentdata = [tuple(line.strip().split('\t')) for line in open('emptyschooltest.txt','r')]
 
 #From studentdata, create set of distinct students and teachers.
 studentlist = set((i[0] for i in studentdata[1:]))
@@ -15,6 +18,15 @@ gradelist = set((i[2] for i in studentdata[1:]))
 gpalist = [int(i[1]) for i in studentdata[1:]]
 
 
+#Check for Empty School
+def emptycheck():
+    if not studentlist or not teacherlist:
+        if not studentlist:
+            print ('This school has no students!')
+        if not teacherlist:
+            print ('This school has no teachers!')
+        sys.exit()
+
 #Create Intro Report
 def createintroreport():
     """print student count, teacher count, and average gpa"""
@@ -24,7 +36,6 @@ def createintroreport():
     print ('There are {0} unique students that attend this school.'.format(len(studentlist)))
     print ('There are {0} unique teachers that attend this school.'.format(len(teacherlist)))
     print ('On average, students at this school maintain a {0} gpa.'.format(sum(gpalist)/len(gpalist)))
-    reportinput = input("If you would like a grade report broken down by grade level, please enter 'grade'. \nIf you would like a grade report broken down by teacher, please enter 'teacher'. \nIf you would like exit, please enter 'exit'")
     print ('-'*50)
 
 #For the secondary reports, we will opt to not use input in the following functions but instead use the global variables we already have
@@ -33,7 +44,6 @@ def creategradeeport():
     print ('  Grade ---------- Average GPA')    
     for i in sorted(gradelist, key = lambda x: int(x) if x.isdigit() else -999):
         grades = [int(y[1]) for y in studentdata[1:] if y[2] == i]
-        #print ("The average GPA for grade {0} is {1}".format(i, sum(grades)/len(grades)))
         print ('    {0}{1}{2}'.format(i, ' '*(18-len(i)), str(sum(grades)/len(grades))[0:5]))
     print ('-----------------------------------------------------------------------------')
         
@@ -52,16 +62,38 @@ def createteacherreport():
         teachernamelength = len(y[1])
         grandenamelength = len(y[0])
         print ('    {0}{1}{2}{3}{4}'.format(y[0], ' '*(15-grandenamelength) , y[1], ' '*(24-teachernamelength), str(sum(grades)/len(grades))[0:5]))
-        #print ('Mr/Mrs. {0} teaches {1} grade and students in his/her class earn an average GPA of {2}'.format(y[1], y[0], sum(grades)/len(grades)))
+
+
+def addstudent():
+    studentname = input('Please enter new student''s name \n')
+    students 
+
+
+def userprompt():
+    """this function will be called as part of the module to intake user input and cal other functions"""
+    inputquestion = 'If you would like a grade report broken down by grade level, please enter ''grade''. \n'
+    inputquestion += 'If you would like a grade report broken down by teacher, please enter ''teacher''. \n'
+    inputquestion += 'If you would like exit, please enter ''exit''. \n'
+    reportinput = input(inputquestion)
+    if reportinput == 'grade':
+        createteacherreport()
+    elif reportinput == 'teacher':
+        createteacherreport()
+    elif reportinput == 'exit':
+        sys.exit()
+    else:
+        print ('Please enter a valid input as instructed above')
+        userprompt()
+
+    
         
 
 
 if __name__ == '__main__':
+    emptycheck()
     createintroreport()
-    inputquestion = 'If you would like a grade report broken down by grade level, please enter ''grade''. \n'
-    inputquestion += 'If you would like a grade report broken down by teacher, please enter ''teacher''. \n''
-    inputquestion += 'If you would like exit, please enter ''exit'''
-    reportinput = input(inputquestion)
+    userprompt()
+
 
 #creategradeeport()
 #createteacherreport()
